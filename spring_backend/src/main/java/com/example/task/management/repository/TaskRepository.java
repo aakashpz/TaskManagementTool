dd1634649e90f5eeaec4124462a161debcdd515b
Aakash Patel <64808721+aakashpz@users.noreply.github.com>
2022-06-20 17:30:56 -0400

    Update TaskRepository.java

--- spring_backend/src/main/java/com/example/task/management/repository/TaskRepository.java
+++ spring_backend/src/main/java/com/example/task/management/repository/TaskRepository.java
@@ -7,5 +7,8 @@ import com.example.task.management.model.Task;
 
 @Repository
 public interface TaskRepository extends MongoRepository<Task, String>{
-	
+	@Query("{'date' : { '$lt' : ?0 }}")
+	List<Task> findOverdueTasks(Date date);
+	@Query("{'date' : { '$gt': ?0, '$lt' : ?1 }}")
+	List<Task> findDueTasks(Date now, Date tomorrow);
 }
