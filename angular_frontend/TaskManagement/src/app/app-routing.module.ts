a5f84a03e62944e9445e687cb11f5758f8344a95
Aakash Patel <64808721+aakashpz@users.noreply.github.com>
2022-06-20 19:27:29 -0400

    Update app-routing.module.ts

--- angular_frontend/TaskManagement/src/app/app-routing.module.ts
+++ angular_frontend/TaskManagement/src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddTaskComponent } from './components/add-task/add-task.component';
 import { TaskDetailsComponent } from './components/task-details/task-details.component';
+import { TaskDueComponent } from './components/task-due/task-due.component';
 import { TaskListComponent } from './components/task-list/task-list.component';
+import { TaskOverdueComponent } from './components/task-overdue/task-overdue.component';
 import { UpdateTaskComponent } from './components/update-task/update-task.component';
 
 const routes: Routes = [
@@ -11,6 +13,8 @@ const routes: Routes = [
   { path: 'add', component: AddTaskComponent },
   { path: 'update/:_id', component: UpdateTaskComponent },
   { path: 'details/:_id', component: TaskDetailsComponent },
+  { path: 'due', component: TaskDueComponent},
+  { path: 'overdue', component: TaskOverdueComponent},
 ];
 
 @NgModule({
